4bae422a821be58f0e820661f980a1c19841b87f
Ana Barbiero <anacarolinabarbiero@gmail.com>
2024-12-03 10:51:49 -0400

    Update desafio1.py
    Melhora no código

--- Listas/Dicionarios/desafios-dicionario/desafio1.py
+++ Listas/Dicionarios/desafios-dicionario/desafio1.py
@@ -6,13 +6,6 @@ usuário um valor em Reais e uma moeda para conversão, e exiba sua taxa de conv
 
 '''
 
-'''
-Desafio 1
-Conversor de moedas
-Crie um dicionário para armazenar taxas de câmbio (por exemplo: 'USD': 5.0, 'EUR': 6.0). Pergunte ao 
-usuário um valor em Reais e uma moeda para conversão, e exiba sua taxa de conversão
-
-'''
 while True: 
     valores = {
         'USD' : 5.0,
@@ -22,13 +15,22 @@ while True:
 
     real = float(input('Digite o valor em real que deseja converter: '))
 
+    for chave, valor in valores.items():
+        print(f'\n{chave}: {valor}\n')
+
     convert = input('Digite para qual moeda você deseja converter: ').upper()
+
+
+    
 
     if convert in valores:
         taxa = valores[convert]
-        print(f'O valor {real} convertido será de: {real * taxa}')
+        print(f'O valor {real} convertido será de: {real / taxa:.2f}')
 
     else:
         print('A moeda não esta na lista.')
+        break
+
+
 
 

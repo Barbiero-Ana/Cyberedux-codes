d37544ea58c87f6bbc59426aacbbe908c0b3a722
Ana Barbiero <anacarolinabarbiero@gmail.com>
2024-12-03 10:37:02 -0400

    Update desafio1.py
    Adição do código feito em sala - Cyberedux

--- Listas/Dicionarios/desafios-dicionario/desafio1.py
+++ Listas/Dicionarios/desafios-dicionario/desafio1.py
@@ -5,3 +5,30 @@ Crie um dicionário para armazenar taxas de câmbio (por exemplo: 'USD': 5.0, 'E
 usuário um valor em Reais e uma moeda para conversão, e exiba sua taxa de conversão
 
 '''
+
+'''
+Desafio 1
+Conversor de moedas
+Crie um dicionário para armazenar taxas de câmbio (por exemplo: 'USD': 5.0, 'EUR': 6.0). Pergunte ao 
+usuário um valor em Reais e uma moeda para conversão, e exiba sua taxa de conversão
+
+'''
+while True: 
+    valores = {
+        'USD' : 5.0,
+        'EUR' : 6.0,
+        'PESO ARGENTINO' : 166.46,
+    }
+
+    real = float(input('Digite o valor em real que deseja converter: '))
+
+    convert = input('Digite para qual moeda você deseja converter: ').upper()
+
+    if convert in valores:
+        taxa = valores[convert]
+        print(f'O valor {real} convertido será de: {real * taxa}')
+
+    else:
+        print('A moeda não esta na lista.')
+
+

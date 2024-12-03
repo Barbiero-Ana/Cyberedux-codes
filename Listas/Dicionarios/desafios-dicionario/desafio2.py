7e705a385521e12bf04867543babcbc459fce0c4
Ana Barbiero <anacarolinabarbiero@gmail.com>
2024-12-03 10:38:18 -0400

    Add files via upload

---
+++ Listas/Dicionarios/desafios-dicionario/desafio2.py
@@ -0,0 +1,9 @@
+'''
+Desafio 2
+Análise de Dados Textuais
+Dado um texto fornecido pelo usuário, crie um programa que:
+Conte a frequência de cada palavra no texto.
+Exiba as 3 palavras mais usadas.
+Identifique e exiba palavras que aparecem apenas uma vez
+'''
+
